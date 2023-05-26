3ebdc0e8f12c56c8f19e06df9b24f7570bc6b2f3
Jose luis <boxixz0991@gmail.com>
2023-05-26 12:17:49 +0200

    finxing header, footer and changing icons for kills

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-const toggle     = document.getElementById('header-tggle');
-const navMenu    = document.getElementById('nav-menu');
-const navClose   = document.getElementById('nav-close');
+const toggle = document.querySelector("#header-tggle");
+const navMenu = document.querySelector("#nav-menu");
+const navClose = document.querySelector("#nav-close");
+const form = document.querySelector(".form");
 
-const cargarEventos = ()=>{
-    toggle.addEventListener('click',motrarMenu);
-    navClose.addEventListener('click',ocultarMenu);
-    
-}
-const motrarMenu =()=>{
-    navMenu.classList.add('show-menu');   
-}
-const ocultarMenu =()=>{
-    navMenu.classList.remove('show-menu');
-}
-
-cargarEventos();
-
+const motrarMenu = () => {
+  navMenu.classList.add("show-menu");
+};
+const ocultarMenu = () => {
+  navMenu.classList.remove("show-menu");
+};
+toggle.addEventListener("click", motrarMenu);
+navClose.addEventListener("click", ocultarMenu);
 
 // MODAL SECTION
 
-const modalViews = document.querySelectorAll('.portfolio__modal');
-const modalCloses = document.querySelectorAll('.portfolio__modal-close');
-const modalBts= document.querySelectorAll('.portfolio-card__buttom');
+const modalViews = document.querySelectorAll(".portfolio__modal");
+const modalCloses = document.querySelectorAll(".portfolio__modal-close");
+const modalBts = document.querySelectorAll(".portfolio-card__buttom");
 
-let modal = function(modalClick){
-    modalViews[modalClick].classList.add('active-modal');
-}
-modalBts.forEach((e,i) => {
-    e.addEventListener('click',()=>{
-        modal(i)
-    })
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+modalBts.forEach((e, i) => {
+  e.addEventListener("click", () => {
+    modal(i);
+  });
 });
 
-modalCloses.forEach(close =>{
-    close.addEventListener('click',()=>{
-        modalViews.forEach(view =>{
-            view.classList.remove('active-modal');
-        })
-    })
-})
+modalCloses.forEach((close) => {
+  close.addEventListener("click", () => {
+    modalViews.forEach((view) => {
+      view.classList.remove("active-modal");
+    });
+  });
+});
+const handelSutmit = (e) => {
+  e.preventDefault();
+  console.log("desde handelSutmit");
+};
+form.addEventListener("submit", handelSutmit);
